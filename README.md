7cb96f082888b31d7b97b6f1b7b6fcc199fc20a5
Jamie Little <jamielittle@Jamies-Mac-mini.local>
2025-08-24 20:21:04 +0100

    Initial commit of node api - also test page for CORS

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tidytasks-fe-api
 Node API project for TidyTasks
+
+##Testing CORS
+
+There is a CORS test page called cors-test.html in the root of /src
---
+++ src/tidytasks-api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const server = express();
+
+console.log('Starting up TidyTasks API');
+
+// Start the webserver, listening on port 4040 (may change later)
+server.listen(4040, () => {
+    console.log('TidyTasks API has started on port 4040')
+})
+
+// Listener for /helloworld endpoint
+server.get('/helloworld', (req, res) => {
+    res.send('Hello from TidyTasks API')
+})
+
+server.get('/cors', (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://tidytasks.local.com');
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.send('Response from CORS test');
+});
