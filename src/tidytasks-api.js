b8368ce4ba29b9ea04fb89b47d2bfdf61528880e
Jamie Little <jamielittle@Jamies-Mac-mini.local>
2025-08-24 23:11:59 +0100

    Improved CORS so that every request has protection

--- src/tidytasks-api.js
+++ src/tidytasks-api.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
 const server = express();
 
 console.log('Starting up TidyTasks API');
+
+console.log('Configuring CORS');
+const options = {
+    origin: 'http://tidytasks.local.com:8080',
+};
+server.use(cors(options));
 
 // Start the webserver, listening on port 4040 (may change later)
 server.listen(4040, () => {
@@ -14,7 +22,7 @@ server.get('/helloworld', (req, res) => {
 })
 
 server.get('/cors', (req, res) => {
-    res.set('Access-Control-Allow-Origin', 'http://tidytasks.local.com');
-    res.set('Access-Control-Allow-Methods', 'GET');
+    //res.set('Access-Control-Allow-Origin', 'http://tidytasks.local.com');
+    //res.set('Access-Control-Allow-Methods', 'GET');
     res.send('Response from CORS test');
 });
